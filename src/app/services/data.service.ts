f167c14d66b4dba7ffb53823157600a1838a9d26
acenaga <mundofuturoca@gmail.com>
2019-11-10 14:37:22 -0300

    Se realizo el componente de skelton text en la seccion de segment y se creo la apgina de Slides para implementar el uso de slides en la aplicacion

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Componente } from '../interfaces/interfaces';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -23,7 +24,10 @@ getAlbumnes() {
 }
 
 getHeroes() {
-  return this.http.get('/assets/data/superheroes.json');
+  return this.http.get('/assets/data/superheroes.json')
+  .pipe(
+    delay(2000)
+  );
 }
 
 
